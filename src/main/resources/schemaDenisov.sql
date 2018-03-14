6b767fe3492e0a7af34d28ca1e8247e21e00024d
Fargys <evgenij.denisov.92@mail.ru>
2018-03-14 14:39:19 +0300

    Fix schema

--- src/main/resources/schemaDenisov.sql
+++ src/main/resources/schemaDenisov.sql
@@ -29,6 +29,8 @@ CREATE TABLE IF NOT EXISTS User (
     phone               VARCHAR(45) NOT NULL,
     is_identified       TINYINT(1) DEFAULT 0,
     office_id           INTEGER NOT NULL,
+    doc_id              INTEGER NOT NULL,
+    citizenship_id      INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Login (
@@ -53,34 +55,17 @@ CREATE TABLE IF NOT EXISTS Citizenship (
     citizenship_code    INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS User_Document (
-    user_id             INTEGER NOT NULL,
-    doc_id              INTEGER NOT NULL,
-);
-
-CREATE TABLE IF NOT EXISTS User_Citizenship (
-    user_id             INTEGER NOT NULL,
-    citizenship_id      INTEGER NOT NULL,
-);
-
 CREATE INDEX Org_Id ON Office (org_id);
 ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
 
 CREATE INDEX Office_Id ON User (office_id);
 ALTER TABLE User ADD FOREIGN KEY (office_id) REFERENCES Office(id);
 
+CREATE INDEX Doc_id ON User (doc_id);
+ALTER TABLE User ADD FOREIGN KEY (doc_id) REFERENCES Document(id);
+
+CREATE INDEX Citizenship_id ON User (citizenship_id);
+ALTER TABLE User ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
+
 CREATE INDEX User_id ON Login (user_id);
 ALTER TABLE Login ADD FOREIGN KEY (user_id) REFERENCES User(id);
-
-CREATE INDEX User_Document_Id ON User_Document (user_id);
-ALTER TABLE User_Document ADD FOREIGN KEY (user_id) REFERENCES User(id);
-
-CREATE INDEX Document_User_Id ON User_Document (doc_id);
-ALTER TABLE User_Document ADD FOREIGN KEY (doc_id) REFERENCES Document(id);
-
-CREATE INDEX User_Citizenship_Id ON User_Citizenship (user_id);
-ALTER TABLE User_Citizenship ADD FOREIGN KEY (user_id) REFERENCES User(id);
-
-CREATE INDEX Citizenship_User_Id ON User_Citizenship (citizenship_id);
-ALTER TABLE User_Citizenship ADD FOREIGN KEY (citizenship_id) REFERENCES Citizenship(id);
-
